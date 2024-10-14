eccfcab46bc6c00e1fb09de9714206226e33b125
Sheikh <sheikhirtiza444@gmail.com>
2024-10-14 10:34:36 +0530

    update and add some files

--- book_recommendation_system.py
+++ book_recommendation_system.py
@@ -76,29 +76,31 @@ def visualize_user_book_matrix_altair(matrix):
 
 # Streamlit UI
 st.sidebar.title('Navigation')
-page = st.sidebar.radio('Go to', ['Home', 'Recommendations', 'Matrix Heatmap'])
+page = st.sidebar.radio('Go to', ['Home', 'Recommendations'])
+
+# Add a header image
+st.markdown(
+    """
+    <div style="text-align: center;">
+        <img src="https://th.bing.com/th/id/OIP.EnOy3-5IcV9Zi9yJRYdATgAAAA?rs=1&pid=ImgDetMain" alt="Header Image" style="width:50%; border-radius: 10px;">
+    </div>
+    """,
+    unsafe_allow_html=True
+)
 
 if page == 'Home':
-    #
+    st.title('Book Recommendation System')
     st.markdown(
         """
-        <div class='center-content'>
-            <img src='https://th.bing.com/th/id/OIP.EnOy3-5IcV9Zi9yJRYdATgAAAA?rs=1&pid=ImgDetMain' alt='Logo' width='400px' style="border-radius: 10px;">
-        </div>
-        """, 
-        unsafe_allow_html=True
-    )
-
-
-    # Floating, multi-colored message
-    st.markdown("""
         <div class="floating-text">
             <h3>Welcome to the Book Recommendation System! Use the sidebar to navigate.</h3>
         </div>
-        """, unsafe_allow_html=True)
+        """,
+        unsafe_allow_html=True
+    )
 
 elif page == 'Recommendations':
-    st.title('📖 Book Recommendations')
+    st.title('Book Recommendations')
     # Load data
     book_df, ratings_df, user_df = load_data()
 
@@ -112,22 +114,18 @@ elif page == 'Recommendations':
     book_id = st.number_input('Enter the unique ID of a book to get recommendations:', min_value=0, max_value=len(book_user_rating['unique_id_book'].unique()) - 1)
     top_n = st.number_input('Number of recommendations to show:', min_value=1, max_value=10, value=3)
 
-    if st.button('📚 Get Recommendations', key='recommend_button'):
+    if st.button('Get Recommendations', key='recommend_button'):
         top_indexes = top_cosine_similarity(Vt.T[:, :50], book_id, top_n)
         recommendations = similar_books(book_user_rating, book_id, top_indexes)
         
         # Display recommendations in a table
         st.write(pd.DataFrame(recommendations))
-
-elif page == 'Matrix Heatmap':
-    st.title('🗺️ User-Book Matrix Heatmap')
-    # Load data
-    book_df, ratings_df, user_df = load_data()
-
-    # Preprocess data
-    book_user_rating, user_book_matrix = preprocess_data(book_df, ratings_df, user_df)
-
-    if st.button('Show Heatmap', key='heatmap_button'):
+        
+        # Show heatmap of the user-book matrix
+        st.markdown('### User-Book Matrix Heatmap')
+        st.write('Here’s a sample of the user-book rating matrix heatmap based on the current dataset:')
+        
+        # Visualize the matrix as a heatmap
         visualize_user_book_matrix_altair(user_book_matrix)
 
 # Custom CSS for styling with yellow and black combination
@@ -177,14 +175,6 @@ st.markdown(
         border-radius: 4px;
         cursor: pointer;
     }
-    button[data-testid="heatmap_button"] {
-        background-color: yellow;
-        color: black;
-        border: none;
-        padding: 10px 20px;
-        border-radius: 4px;
-        cursor: pointer;
-    }
     button:hover {
         background-color: black;
         color: yellow;
@@ -198,4 +188,6 @@ st.markdown(
 st.sidebar.title('Feedback')
 feedback = st.sidebar.text_area('Your feedback:')
 if st.sidebar.button('Submit'):
+    with open('feedback.txt', 'a') as f:
+        f.write(feedback + '\n')
     st.sidebar.write('Thank you for your feedback!')
